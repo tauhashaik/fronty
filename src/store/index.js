8622f7272ed0e8aae3c45a1a7df9ff55f2b0fb80
tauhashaik <hoosains19@gmail.com>
2024-02-29 14:53:41 +0200

    cars

--- src/store/index.js
+++ src/store/index.js
@@ -23,7 +23,19 @@ export default createStore({
         console.error('Error fetching products',error)
       }
     },
-
+    async addProd({commit},newProduct){
+      const {data} = await axios.post(baseUrl+'/product/add',newProduct);
+      window.location.reload();
+      console.log(data);
+    },
+    async deleteProd({commit},id){
+      await axios.delete(baseUrl+ '/product/delete/' +id);
+      window.location.reload();
+    },
+    async updateProd({commit},update){
+      await axios.patch(baseUrl+ '/product/update/'+update.id, update)
+      window.location.reload();
+    }
   },
   modules: {
   }
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -5,26 +5,102 @@
     <div>
         <h1 id="title">Admin</h1>
 
+        <button id="addButt" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Add
+        </button>
         <table class="table table-dark">
             <tr>
                 <th style="color: white; background-color: gold;">id</th>
                 <th style="color: white; background-color: indigo;">prodName</th>
                 <th style="color: white; background-color: gold;">category</th>
                 <th style="color: white; background-color: indigo;">amount</th>
-                <th style="color: white; background-color: indigo;">ProdURL</th>
-                <th style="color: white; background-color: gold;">quantity</th>
+                <th style="color: white; background-color: gold;">ProdURL</th>
+                <th style="color: white; background-color: indigo;">quantity</th>
               
             </tr>
-
-            <tr v-for="item in $store.state.product" :key="item.ID">
+            
+            <tr v-for="item in $store.state.product" :key="item.id">
                 <td style = "color: white; background-color: indigo; padding-top: 40px">{{item.prodID}}</td>
                 <td style = "color: white; background-color: black; font-size: 20px; padding-top: 40px">{{item.prodName}}</td>
                 <td style = "color: white; background-color: indigo; font-size: 20px; padding-top: 40px">{{ item.category }}</td>
                 <td style = "color: white; background-color: black; font-size: 20px; padding-top: 40px">R{{item.amount}}</td>
                 <td style = "color: white; background-color:indigo"><img id='adminImg' :src=item.prodUrl></td>
                 <td style = "color: white; background-color: black; font-size: 20px; padding-top: 40px">{{item.quantity}}</td>
-            </tr>    
+                <td><button @click="deleteProd(item.prodID)" id="deleteButt">Delete</button></td>
+
+                <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2" id="editButt">
+                  Edit
+                </button></td>
+            </tr>   
         </table>
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div id="modalBody" class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">ADD PRODUCT</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+
+                <p id= "modalP">Product Name</p>
+                <input id="input1" placeholder="prodName" type="text" class="form-control" v-model="prodName" required>
+
+                <p id= "modalP">Quantity</p>
+                <input id="input2" placeholder="quantity" type="number" class="form-control" v-model="quantity" required>
+
+                <p id= "modalP">Amount</p>
+                <input id="input3" placeholder="amount" type="number" class="form-control" v-model="amount" required>
+
+                <p id= "modalP">Category</p>
+                <input id="input4" placeholder="category" type="text" class="form-control" v-model="category" required>
+
+                <p id= "modalP">Product Url</p>
+                <input id="input4" placeholder="prodUrl" type="text" class="form-control" v-model="prodUrl" required>
+      </div>
+      <div id="modalFoot" class="modal-footer">
+        <button id="closeButt" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button @click= "addProd()" id="saveButt" type="button" class="btn btn-primary">Save Product</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Button trigger modal -->
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div id="modalBody" class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">EDIT PRODUCT</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+
+                <p id= "modalP">Product Name</p>
+                <input id="input1" placeholder="prodName" type="text" class="form-control" v-model="prodName" required>
+
+                <p id= "modalP">Quantity</p>
+                <input id="input2" placeholder="quantity" type="number" class="form-control" v-model="quantity" required>
+
+                <p id= "modalP">Amount</p>
+                <input id="input3" placeholder="amount" type="number" class="form-control" v-model="amount" required>
+
+                <p id= "modalP">Category</p>
+                <input id="input4" placeholder="category" type="text" class="form-control" v-model="category" required>
+
+                <p id= "modalP">Product Url</p>
+                <input id="input4" placeholder="prodUrl" type="text" class="form-control" v-model="prodUrl" required>
+      </div>
+      <div class="modal-footer">
+        <button id="closeButt" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button @click="updateProd()" id="saveButt" type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
         
     </div>
 </template>
@@ -32,19 +108,36 @@
 export default {
     data(){
         return{
-        "prodName": null,
-        "quantity": null,
-        "amount": null,
-        "category": null,
-        "prodUrl": null
+        prodName: null,
+        quantity: null,
+        amount: null,
+        category: null,
+        prodUrl: null
         }
     },
     methods:{
-
+        addProd(){
+            this.$store.dispatch('addProd',this.$data)
+        },
+        deleteProd(id){
+            this.$store.dispatch('deleteProd',id)
+        },
+        updateProd(id){
+            let edit = {
+                id:id,
+                prodName:this.prodName,
+                quantity:this.quantity,
+                amount:this.amount,
+                category:this.categpry,
+                prodUrl:this.prodUrl
+            }
+            this.$store.dispatch('updateProd',edit)
+        }
     },
     computed:{
         getProduct(){
             this.$store.dispatch('getProduct')
+
         }
     },
     mounted(){
@@ -59,7 +152,7 @@ export default {
 #title{
     margin-top: 100px;
     font-family: audiowide;
-    color: gold;
+    color: indigo;
 }
 
 #adminBody{
@@ -113,5 +206,84 @@ export default {
     background-color: red;
     color: black;
 }
-    
+#modalBody{
+    background-color: indigo;
+    color: gold;
+    font-family: audiowide;
+}
+#closeButt{
+    background-color: red;
+    color: black;
+}
+#closeButt:hover{
+    background-color: red;
+    color: black;
+    transform: scale(1.1);
+    transition: 0.2s;
+}
+#saveButt{
+    background-color: gold;
+    color: black;
+}
+#saveButt:hover{
+    background-color: gold;
+    color: black;
+    transform: scale(1.1);
+    transition: 0.2s;
+}
+
+#addButt{
+    background-color: indigo;
+    margin-bottom: 20px;
+    color: gold;
+}
+
+#deleteButt{
+    background-color: red;
+    color: black;
+    border-radius: 12px;
+}
+#deleteButt:hover{
+    background-color: red;
+    color: black;
+    border-radius: 12px;
+    transform: scale(1.1);
+    transition: 0.2s;
+}
+#editButt{
+    background-color: red;
+    color: black;
+    border-radius: 12px;
+}
+#editButt:hover{
+    background-color: red;
+    color: black;
+    border-radius: 12px;
+    transform: scale(1.1);
+    transition: 0.2s;
+}
+
+#modalP{
+    color: gold;
+    font-family: Audiowide;
+    margin-top: 10px;
+    text-shadow: 0px 0px 10px white !important;
+    font-size: large;
+}
+#input1{
+    border: solid 2px gold ;
+    box-shadow: 0px 0px 7px gold;
+}
+#input2{
+    border: solid 2px gold;
+    box-shadow: 0px 0px 7px gold;
+}
+#input3{
+    border: solid 2px gold;
+    box-shadow: 0px 0px 7px gold;
+}
+#input4{
+    border: solid 2px gold;
+    box-shadow: 0px 0px 7px gold;
+} 
 </style>
